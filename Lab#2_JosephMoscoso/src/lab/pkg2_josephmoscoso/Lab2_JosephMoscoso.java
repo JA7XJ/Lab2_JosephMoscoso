bcf833bdcbe8c349c19c5c4362ef2573779e5cfe
Your Name <josephmoscoso70@hotmail.com>
2018-05-04 14:06:01 -0600

    menu creado

--- Lab#2_JosephMoscoso/src/lab/pkg2_josephmoscoso/Lab2_JosephMoscoso.java
+++ Lab#2_JosephMoscoso/src/lab/pkg2_josephmoscoso/Lab2_JosephMoscoso.java
@@ -1,21 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lab.pkg2_josephmoscoso;
-
-/**
- *
- * @author Joseph
- */
+import java.util.*;
 public class Lab2_JosephMoscoso {
-
-    /**
-     * @param args the command line arguments
-     */
+static Scanner sc=new Scanner(System.in);
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
